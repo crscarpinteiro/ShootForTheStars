f939f386843271d78611f5420270eba5c25c5522
crscarpinteiro <30414551+crscarpinteiro@users.noreply.github.com>
2018-11-08 16:25:23 +0000

    Challenge 's starter kit

---
+++ starting.py
@@ -0,0 +1,162 @@
+import os
+import numpy as np
+import scipy.stats as spstat
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from astropy.table import Table
+
+
+obj1 = Table.read('plasticc-kit/data/fake010.csv', format='csv')
+print(obj1)
+
+class LightCurve(object):
+    '''Light curve object for PLAsTiCC formatted data'''
+    
+    _passbands = OrderedDict([(0,'C4'),\
+                              (1,'C2'),\
+                              (2,'C3'),\
+                              (3,'C1'),\
+                              (4,'k'),\
+                              (5,'C5')])
+    
+    _pbnames = ['u','g','r','i','z','y']
+    
+    def __init__(self, filename):
+        '''Read in light curve data'''
+
+        self.DFlc     = Table.read(filename, format='ascii.csv')
+        self.filename = filename.replace('.csv','')
+        self._finalize()
+     
+    # this is some simple code to demonstrate how to calculate features on these multiband light curves
+    # we're not suggesting using these features specifically
+    # there also might be additional pre-processing you do before computing anything
+    # it's purely for illustration
+    def _finalize(self):
+        '''Store individual passband fluxes as object attributes'''
+        # in this example, we'll use the weighted mean to normalize the features
+        weighted_mean = lambda flux, dflux: np.sum(flux*(flux/dflux)**2)/np.sum((flux/dflux)**2)
+        
+        # define some functions to compute simple descriptive statistics
+        normalized_flux_std = lambda flux, wMeanFlux: np.std(flux/wMeanFlux, ddof = 1)
+        normalized_amplitude = lambda flux, wMeanFlux: (np.max(flux) - np.min(flux))/wMeanFlux
+        normalized_MAD = lambda flux, wMeanFlux: np.median(np.abs((flux - np.median(flux))/wMeanFlux))
+        beyond_1std = lambda flux, wMeanFlux: sum(np.abs(flux - wMeanFlux) > np.std(flux, ddof = 1))/len(flux)
+        
+        for pb in self._passbands:
+            ind = self.DFlc['passband'] == pb
+            pbname = self._pbnames[pb]
+            
+            if len(self.DFlc[ind]) == 0:
+                setattr(self, f'{pbname}Std', np.nan)
+                setattr(self, f'{pbname}Amp', np.nan)
+                setattr(self, f'{pbname}MAD', np.nan)
+                setattr(self, f'{pbname}Beyond', np.nan)
+                setattr(self, f'{pbname}Skew', np.nan)
+                continue
+            
+            f  = self.DFlc['flux'][ind]
+            df = self.DFlc['flux_err'][ind]
+            m  = weighted_mean(f, df)
+            
+            # we'll save the measurements in each passband to simplify access.
+            setattr(self, f'{pbname}Flux', f)
+            setattr(self, f'{pbname}FluxUnc', df)
+            setattr(self, f'{pbname}Mean', m)
+            
+            # compute the features
+            std = normalized_flux_std(f, df)
+            amp = normalized_amplitude(f, m)
+            mad = normalized_MAD(f, m)
+            beyond = beyond_1std(f, m)
+            skew = spstat.skew(f) 
+            
+            # and save the features
+            setattr(self, f'{pbname}Std', std)
+            setattr(self, f'{pbname}Amp', amp)
+            setattr(self, f'{pbname}MAD', mad)
+            setattr(self, f'{pbname}Beyond', beyond)
+            setattr(self, f'{pbname}Skew', skew)
+        
+        # we can also construct features between passbands
+        pbs = list(self._passbands.keys())
+        for i, lpb in enumerate(pbs[0:-1]):
+            rpb = pbs[i+1]
+            
+            lpbname = self._pbnames[lpb]
+            rpbname = self._pbnames[rpb]
+            
+            colname = '{}Minus{}'.format(lpbname, rpbname.upper())
+            lMean = getattr(self, f'{lpbname}Mean', np.nan)
+            rMean = getattr(self, f'{rpbname}Mean', np.nan)
+            col = -2.5*np.log10(lMean/rMean) if lMean> 0 and rMean > 0 else -999
+            setattr(self, colname, col)
+    
+    # this is a simple routine to visualize a light curve
+    # it can plot vs the MJD array of the light curve
+    # or vs an optional `phase` array that you pass 
+    def plot_multicolor_lc(self, phase=None):
+        '''Plot the multiband light curve'''
+
+        fig, ax = plt.subplots(figsize=(8,6))
+
+        
+        if phase is None:
+            phase = []
+        if len(phase) != len(self.DFlc):
+            phase = self.DFlc['mjd']
+            xlabel = 'MJD'
+        else:
+            xlabel = 'Phase'
+            
+        for i, pb in enumerate(self._passbands):
+            pbname = self._pbnames[pb]
+            ind = self.DFlc['passband'] == pb
+            if len(self.DFlc[ind]) == 0:
+                continue
+            ax.errorbar(phase[ind], 
+                     self.DFlc['flux'][ind],
+                     self.DFlc['flux_err'][ind],
+                     fmt = 'o', color = self._passbands[pb], label = f'{pbname}')
+        ax.legend(ncol = 4, frameon = True)
+        ax.set_xlabel(f'{xlabel}', fontsize='large')
+        ax.set_ylabel('Flux', fontsize='large')
+        fig.suptitle(self.filename, fontsize='x-large')
+        fig.tight_layout(rect=[0, 0, 1, 0.97])
+    
+    def get_features(self):
+        '''Return all the features for this object'''
+        variables = ['Std', 'Amp', 'MAD', 'Beyond', 'Skew']
+        feats = []
+        for i, pb in enumerate(self._passbands):
+            pbname = self._pbnames[pb]
+            feats += [getattr(self, f'{pbname}{x}', np.nan) for x in variables]
+        return feats
+
+
+# lc = LightCurve('plasticc-kit/data/fake010.csv')
+# lc.plot_multicolor_lc()
+# lc = LightCurve('plasticc-kit/data/fake030.csv')
+# lc.plot_multicolor_lc()
+
+# from gatspy.periodic import LombScargleMultiband
+# model = LombScargleMultiband(fit_period=True)
+
+# # we'll window the search range by setting minimums and maximums here
+# # but in general, the search range you want to evaluate will depend on the data
+# # and you will not be able to window like this unless you know something about
+# # the class of the object a priori
+# t_min = max(np.median(np.diff(sorted(lc.DFlc['mjd']))), 0.1)
+# t_max = min(10., (lc.DFlc['mjd'].max() - lc.DFlc['mjd'].min())/2.)
+
+# model.optimizer.set(period_range=(t_min, t_max), first_pass_coverage=5)
+# model.fit(lc.DFlc['mjd'], lc.DFlc['flux'], dy=lc.DFlc['flux_err'], filts=lc.DFlc['passband'])
+# period = model.best_period
+# print(f'{lc.filename} has a period of {period} days')
+
+# phase = (lc.DFlc['mjd'] /period) % 1
+# lc.plot_multicolor_lc(phase=phase)
+# header = Table.read('plasticc-kit/data/plasticc_training_set_metadata_stub.csv', format='csv')
+# header
+
+
